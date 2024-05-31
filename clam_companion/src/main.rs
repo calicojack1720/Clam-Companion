5316752a956f91ac84120cb17c6954ad15583a2e
Michael Astfalk <mtastfalkjr@verion.net>
2024-05-31 18:09:50 -0400

    Initialized project files

--- clam_companion/src/main.rs
+++ clam_companion/src/main.rs
@@ -1,3 +1,20 @@
+/*
+ Clam Companion
+ Automates running Clam AV scans and creating conf files for Clam AV.
+ Created by calicojack1720
+ Created: 5/30/24
+ Updated: 5/31/24
+*/
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut choice: i16 = -1;
+    let mut choice_input = String::new();
+
+	while choice != 0 {
+		io::stdin()
+			.read_line(&mut choice_input)
+			.expect("Failed to read line");
+	}
 }
---
+++ clam_companion/target/debug/deps/clam_companion-9c60dc6defa88b22.d
@@ -0,0 +1,9 @@
+/home/mtastfalk/rust-programs/Clam-Companion/clam_companion/target/debug/deps/libclam_companion-9c60dc6defa88b22.rmeta: src/main.rs Cargo.toml
+
+/home/mtastfalk/rust-programs/Clam-Companion/clam_companion/target/debug/deps/clam_companion-9c60dc6defa88b22.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
