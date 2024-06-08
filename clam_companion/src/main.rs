1a1b801950b13df0b8d2ecde5d713da1f152fbdd
Michael Astfalk <mtastfalkjr@verion.net>
2024-06-08 11:39:23 -0400

    Started command line menu

--- clam_companion/src/main.rs
+++ clam_companion/src/main.rs
@@ -3,18 +3,26 @@
  Automates running Clam AV scans and creating conf files for Clam AV.
  Created by calicojack1720
  Created: 5/30/24
- Updated: 5/31/24
+ Updated: 6/8/24
 */
 
 use std::io;
+use std::cmp;
 
 fn main() {
-    let mut choice: i16 = -1;
     let mut choice_input = String::new();
 
-	while choice != 0 {
+	println!("Welcome to Clam Companion!");
+	
+	while !(choice_input.eq("0")) {
+
+		println!("\nClam Companion Menu - Please select from the following options:");
+		println!("\n0 - Quit");
+	
 		io::stdin()
 			.read_line(&mut choice_input)
 			.expect("Failed to read line");
+
+		choice_input.pop();
 	}
 }
